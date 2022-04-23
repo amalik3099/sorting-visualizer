d6d9207105ba3645a211d9b72cdbd86584aeca1d
amalik3099 <46609090+amalik3099@users.noreply.github.com>
2022-04-23 17:28:57 -0400

    implemented side bar menu for navigation across application

--- static/layout.js
+++ static/layout.js
@@ -1,11 +1,28 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
 $(document).ready(function () {
-    $(".bar").draggable({
-        revert: "valid",
-        drag: function(event, ui){
-            console.log("start")
-        },
-        stop: function(event, ui){
-            
-            }
-        });
-})
+    $("#open_sidebar").click(function() {
+        openNav()
+    })
+    $(".closebtn").click(function() {
+        closeNav()
+    })
+    $(".home_btn").click(function() {
+        window.location.href = "/"
+    })
+    $(".insertion_btn").click(function() {
+        window.location.href = "/learn/5"
+    })
+    $(".bubble_btn").click(function() {
+        window.location.href = "/learn/1"
+    })
+    $(".quiz_btn").click(function() {
+        window.location.href = "/quiz/1"
+    })
+});
