95b3c8d70225c1640b6df15017c206e3caf2bd79
amalik3099 <46609090+amalik3099@users.noreply.github.com>
2022-04-26 10:45:55 -0400

    removed sidebar and added dropdowns to navbar instead

--- static/layout.js
+++ static/layout.js
@@ -1,28 +1,28 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
+// function openNav() {
+//     document.getElementById("mySidenav").style.width = "250px";
+// }
 
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
+// function closeNav() {
+//     document.getElementById("mySidenav").style.width = "0";
+// }
 
-$(document).ready(function () {
-    $("#open_sidebar").click(function() {
-        openNav()
-    })
-    $(".closebtn").click(function() {
-        closeNav()
-    })
-    $(".home_btn").click(function() {
-        window.location.href = "/"
-    })
-    $(".insertion_btn").click(function() {
-        window.location.href = "/learn/5"
-    })
-    $(".bubble_btn").click(function() {
-        window.location.href = "/learn/1"
-    })
-    $(".quiz_btn").click(function() {
-        window.location.href = "/quiz/1"
-    })
-});
+// $(document).ready(function () {
+//     $("#open_sidebar").click(function() {
+//         openNav()
+//     })
+//     $(".closebtn").click(function() {
+//         closeNav()
+//     })
+//     $(".home_btn").click(function() {
+//         window.location.href = "/"
+//     })
+//     $(".insertion_btn").click(function() {
+//         window.location.href = "/learn/5"
+//     })
+//     $(".bubble_btn").click(function() {
+//         window.location.href = "/learn/1"
+//     })
+//     $(".quiz_btn").click(function() {
+//         window.location.href = "/quiz/1"
+//     })
+// });
