cf6c46fe00b6a47bd2531aae0cf8df85c9b30de2
amalik3099 <46609090+amalik3099@users.noreply.github.com>
2022-04-26 12:13:52 -0400

    added quiz feedback with with questions were right/wrong

--- static/quiz_end.js
+++ static/quiz_end.js
@@ -10,6 +10,15 @@ function calc_score() {
         if (JSON.stringify(correct_ans) == JSON.stringify(user_ans)) {
             console.log("correct!");
             score = score + 1
+            let temp_correct = $('<div>').html('<div id="feedback_q">Question ' + i + '</div><div id="ans_tick">' + '&#10004;' + '</div>')
+            // let new_div = $('<div>').attr('id','name_div').attr('data-name',value).attr('data-parent', cont)
+            // $(new_div).html(value)
+            // $('#step2').html('<span>Step 2</span><img id ="tick" src="../static/data/tick.png" alt="there should" srcset="">')
+            $("#feedback_div").append(temp_correct)
+        }
+        else {
+            let temp_wrong = $('<div>').html('<div id="feedback_q">Question ' + i + '</div><div id="ans_wrong">' + '&times;' + '</div>')
+            $("#feedback_div").append(temp_wrong)    
         }
     }
     $("#score_div").html("You scored " + score + " out of 10 correctly!")
