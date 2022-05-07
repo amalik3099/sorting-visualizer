e93137d917cea444cb0f9394bd021c9c93af1703
SimoneXL <90643296+SimoneXL@users.noreply.github.com>
2022-05-07 16:56:14 -0400

    button and quiz_end change

--- static/quiz_end.js
+++ static/quiz_end.js
@@ -12,8 +12,8 @@ function calc_score() {
         if (JSON.stringify(correct_ans) == JSON.stringify(user_ans)) {
             console.log("correct!");
             score = score + 1
-            if (quiz_data[String(i)]['topic'] == "Bubble Sort") {bubble_score = bubble_score+1}
-            if (quiz_data[String(i)]['topic'] == "Insertion Sort") {insertion_score = insertion_score+1}
+            if (quiz_data[String(i)]['topic'] == "Bubble Sort") { bubble_score = bubble_score + 1 }
+            if (quiz_data[String(i)]['topic'] == "Insertion Sort") { insertion_score = insertion_score + 1 }
             let temp_correct = $('<div>').html('<div id="feedback_q">Question ' + i + '</div><div id="ans_tick">' + '&#10004;' + '</div>')
             // let new_div = $('<div>').attr('id','name_div').attr('data-name',value).attr('data-parent', cont)
             // $(new_div).html(value)
@@ -22,13 +22,13 @@ function calc_score() {
         }
         else {
             let temp_wrong = $('<div>').html('<div id="feedback_q">Question ' + i + '</div><div id="ans_wrong">' + '&times;' + '</div>')
-            $("#feedback_div").append(temp_wrong)    
+            $("#feedback_div").append(temp_wrong)
         }
     }
-    let bubble_overall = (bubble_score/5)*100
-    let insertion_overall = (insertion_score/5)*100
-    let bubble_div = $('<div>').html('Your Bubble Sort Score is: ' + bubble_overall + '%')
-    let insertion_div = $('<div>').html('Your Insertion Sort Score is: ' + insertion_overall + '%')
+    let bubble_overall = (bubble_score / 5) * 100
+    let insertion_overall = (insertion_score / 5) * 100
+    let bubble_div = $('<div>').html('Your Bubble Sort Score on Question 1, 2, 5, 6, 7 is: ' + bubble_overall + '%')
+    let insertion_div = $('<div>').html('Your Insertion Sort Score on Question 2, 3, 8, 9, 10 is: ' + insertion_overall + '%')
     $("#breakdown_div").append(bubble_div)
     $("#breakdown_div").append('<br>')
     $("#breakdown_div").append(insertion_div)
@@ -41,7 +41,7 @@ $(document).ready(function () {
         window.location.href = "/quiz/" + 1
     })
     $("#home_btn").click(function () {
-        window.location.href = "/" 
+        window.location.href = "/"
     })
-    
+
 })
